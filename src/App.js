ba4d97d0d93d38cab09a66df4249ac3ba8c68951
moh7afiz90 <moh.7afiz@gmail.com>
2020-02-22 02:02:44 +0300

    getting user location

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const [lat, setLat] = useState(null)
+  const [errorMessage, setErrorMessage] = useState()
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        setLat(position.coords.latitude)
+      },
+      error => {
+        setErrorMessage(error.message)
+      }
+    )
+  }, [])
+
   return (
     <div>
-      <div>App Component</div>
+      <div>latitude: {lat}</div>
+      <div>Error: {errorMessage}</div>
     </div>
   )
 }
