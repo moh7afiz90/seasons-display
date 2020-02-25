2e7a1ad91f0bf9ec57623f9d4b3194b7f5e9a898
moh7afiz90 <moh.7afiz@gmail.com>
2020-02-25 22:14:38 +0300

    added loader spinner and refactor the code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import SeasonDisplay from './SeasonDisplay/SeasonDisplay'
+import Spinner from './Spinner'
 
 function App() {
   const [lat, setLat] = useState(null)
@@ -16,13 +17,16 @@ function App() {
     )
   }, [])
 
-  if (lat && !errorMessage) {
-    return <SeasonDisplay lat={lat}></SeasonDisplay>
+  const renderContent = () => {
+    if (lat && !errorMessage) {
+      return <SeasonDisplay lat={lat}></SeasonDisplay>
+    }
+    if (!lat && errorMessage) {
+      return <div>Error: {errorMessage}</div>
+    }
+    return <Spinner message="Please accept location request...."></Spinner>
   }
-  if (!lat && errorMessage) {
-    return <div>Error: {errorMessage}</div>
-  }
-  return <div>Loading</div>
+  return <div>{renderContent()}</div>
 }
 
 export default App
